0691771d3767bc69a3ba7d1f83e6caa7d600a965
Paul Anjelyno Bakshi <77683121+AinJex@users.noreply.github.com>
2023-05-10 16:52:23 -0600

    Add files via upload

---
+++ testalphabeta.py
@@ -0,0 +1,250 @@
+import random
+import unittest 
+from connect4 import Board
+from math import inf
+
+
+def alpha_beta(board, player, alpha, beta, ply):
+	"""
+	Function receives an instances of the Board class, the player who is to act at this state (either X or O),
+	the value of alpha, beta, and the maximum search depth given by the variable ply.
+
+	The function returns three values: 
+	1. the score of the optimal move for the player who is to act;
+	2. the optimal move
+	3. the total number of nodes expanded to find the optimal move 
+	"""
+
+	if board.is_terminal():
+
+		if board.game_value() == 1:
+			return 1, 0, 7
+		elif board.game_value() == -1:
+			return -1, 0, 7
+		else:
+			return 0, 0, 7
+
+	elif ply == 0:
+
+		return 0, 0, 0
+
+	if player == 'X':
+		bestScore = -inf
+
+		# score = 0
+		# opt = 0
+		moves = board.available_moves()
+		bestMove = random.choice(moves)
+		nodes = 0
+		for move in moves:
+			if board.allows_move(move):
+				board.perform_move(move, player)
+				# print(board)
+				score, opt, expansions = alpha_beta(board, 'O',alpha,beta, ply - 1)
+				nodes += expansions
+				board.undo_move(move)
+				if score > bestScore:
+					# print(score)
+					# print(bestScore)
+					bestScore = score
+					bestMove = move
+
+				alpha = max(alpha, bestScore)
+				# nodes += expansions
+				if alpha >= beta:
+					break
+
+		return bestScore, bestMove, nodes
+
+	else:
+		bestScore = inf
+
+		score = 0
+		opt = 0
+		moves = board.available_moves()
+		bestMove = random.choice(moves)
+		nodes = 0
+
+		for move in moves:
+			if board.allows_move(move):
+				board.perform_move(move, player)
+				# print(board)
+				score, opt, expansions = alpha_beta(board, 'X',alpha, beta, ply - 1)
+				nodes += expansions
+				board.undo_move(move)
+				# print(score)
+				# print(bestScore)
+				if score < bestScore:
+					bestScore = score
+					bestMove = move
+				beta = min(beta, bestScore)
+				# nodes += expansions
+				if alpha >= beta:
+					break
+
+		return bestScore, bestMove, nodes
+
+
+class TestMinMaxDepth1(unittest.TestCase):
+
+	def test_depth1a(self):
+		b = Board()
+		player = b.create_board('010101')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 1)
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 0)
+		# print(expansions)
+
+	def test_depth1b(self): 
+		b = Board() 
+		player = b.create_board('001122')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 1)
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 3)
+		# print(expansions)
+
+	def test_depth1c(self): 
+		b = Board() 
+		player = b.create_board('335566')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 1)
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 4)
+		# print(expansions)
+
+	def test_depth1d(self):
+		b = Board() 
+		player = b.create_board('3445655606')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 1)
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 6)
+		# print(expansions)
+
+	def test_depth1e(self):
+		b = Board() 
+		player = b.create_board('34232210101')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 1)
+		self.assertEqual(bestScore, -1)
+		self.assertEqual(bestMove, 1)
+		# print(expansions)
+
+	def test_depth1f(self):
+		b = Board() 
+		player = b.create_board('23445655606')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 1)
+		self.assertEqual(bestScore, -1)
+		self.assertEqual(bestMove, 6)
+		# print(expansions)
+
+	def test_depth1g(self): 
+		b = Board() 
+		player = b.create_board('33425614156')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 1)
+		self.assertEqual(bestScore, -1)
+		self.assertEqual(bestMove, 2)
+		# print(expansions)
+
+class TestMinMaxDepth3(unittest.TestCase):
+
+	def test_depth3a(self):
+		b = Board()
+		player = b.create_board('303111426551')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 3)
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 2)
+
+	def test_depth3b(self): 
+		b = Board() 
+		player = b.create_board('23343566520605001')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 3)
+		self.assertEqual(bestScore, -1)
+		self.assertEqual(bestMove, 6)
+
+	def test_depth3c(self): 
+		b = Board() 
+		player = b.create_board('10322104046663')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 3)
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 0)
+
+	def test_depth3d(self):
+		b = Board() 
+		player = b.create_board('00224460026466')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 3)
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 3)
+
+	def test_depth3e(self):
+		b = Board() 
+		player = b.create_board('102455500041526')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 3)
+		self.assertEqual(bestScore, -1)
+		self.assertEqual(bestMove, 1)
+
+	def test_depth3f(self):
+		b = Board() 
+		player = b.create_board('01114253335255')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 3)
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 2)
+
+	def test_depth3g(self): 
+		b = Board() 
+		player = b.create_board('0325450636643')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 3)
+		self.assertEqual(bestScore, -1)
+		self.assertEqual(bestMove, 5)
+
+class TestMinMaxDepth5(unittest.TestCase):
+	def test_depth5a(self):
+		b = Board()
+		player = b.create_board('430265511116')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 5)
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 3)
+		
+	def test_depth5b(self):
+		b = Board()
+		player = b.create_board('536432111330')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 5)
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 5)
+
+	def test_depth5c(self):
+		b = Board()
+		player = b.create_board('322411004326')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 5)
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 3)
+
+	def test_depth5d(self):
+		b = Board()
+		player = b.create_board('3541226000220')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 5)
+		self.assertEqual(bestScore, -1)
+		self.assertEqual(bestMove, 4)
+
+	def test_depth5e(self):
+		b = Board()
+		player = b.create_board('43231033655')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 5)
+		self.assertEqual(bestScore, -1)
+		self.assertEqual(bestMove, 1)
+
+	def test_depth5f(self):
+		b = Board()
+		player = b.create_board('345641411335')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 5)
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 5)
+
+	def test_depth5g(self):
+		b = Board()
+		player = b.create_board('336604464463')
+		bestScore, bestMove, expansions = alpha_beta(b, player, -inf, inf, 5)
+
+		self.assertEqual(bestScore, 1)
+		self.assertEqual(bestMove, 3)			
+		# print(expansions)
+
+if __name__ == '__main__':
+    unittest.main()
